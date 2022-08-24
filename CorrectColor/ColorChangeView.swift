75f7a64f5432c838458a30c974e9ac94dacca4b6
darynamm <darynamanuilenko@gmail.com>
2022-08-24 09:19:08 -0600

    made a random color assesment

--- CorrectColor/ColorChangeView.swift
+++ CorrectColor/ColorChangeView.swift
@@ -14,6 +14,14 @@ struct ColorChangeView: View
         Text("Hello, world!")
             .padding()
     }
+    
+    func makeRandomColor() -> Color
+    {
+        let redPercent   : Double = Double (arc4random()  % UInt32 (256)) / 255.0
+        let greenPercent : Double = Double (arc4random()  % UInt32 (256)) / 255.0
+        let bluePercent  : Double = Double (arc4random()  % UInt32 (256)) / 255.0
+        return Color (red:redPercent, green: greenPercent, blue: bluePercent)
+    }
 }
 
 struct ContentView_Previews: PreviewProvider
