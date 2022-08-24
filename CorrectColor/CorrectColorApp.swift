62f8de6810530fedbc61b0eb2615fb51056636c9
darynamm <darynamanuilenko@gmail.com>
2022-08-24 08:56:04 -0600

    named project and created repo

---
+++ CorrectColor/CorrectColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CorrectColorApp.swift
+//  CorrectColor
+//
+//  Created by Manuilenko, Daryna on 8/24/22.
+//
+
+import SwiftUI
+
+@main
+struct CorrectColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
